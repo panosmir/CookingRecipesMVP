71d3920712d6aa7f15d50713b27a7c5d04f4cfdf
Panos <panagiotis.mirmigkis@gmail.com>
2017-06-14 18:47:25 +0300

    (Previous commit)

--- app/src/main/java/com/mir/panosdev/cookingrecipesmvp/mvp/view/DetailsActivityMVP.java
+++ app/src/main/java/com/mir/panosdev/cookingrecipesmvp/mvp/view/DetailsActivityMVP.java
@@ -1,7 +1,11 @@
 package com.mir.panosdev.cookingrecipesmvp.mvp.view;
 
 import com.mir.panosdev.cookingrecipesmvp.base.BaseView;
+import com.mir.panosdev.cookingrecipesmvp.mvp.model.category.Category;
+import com.mir.panosdev.cookingrecipesmvp.mvp.model.ingredient.Ingredient;
 import com.mir.panosdev.cookingrecipesmvp.mvp.model.recipes.Recipe;
+
+import java.util.List;
 
 /**
  * Created by Panos on 4/17/2017.
@@ -19,6 +23,16 @@ public interface DetailsActivityMVP extends BaseView {
         boolean getUpdateSignal();
 
         void onUpdateShowToast(String message);
+
+        int getCategoryId();
+
+        void onClearIngredients();
+
+        void onIngredientsLoaded(List<Ingredient> ingredientList);
+
+        void onClearItems();
+
+        void onItemsLoaded(List<Category> categories);
     }
 
     interface Presenter {
