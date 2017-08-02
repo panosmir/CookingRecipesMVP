4ea4d49e6cd7e9edb449e81c7970b8081be1a648
Panos <panagiotis.mirmigkis@gmail.com>
2017-08-02 11:37:59 +0300

    Fix the NPE if search results is null.

--- app/src/main/java/com/mir/panosdev/cookingrecipesmvp/mvp/presenter/SearchPresenter.java
+++ app/src/main/java/com/mir/panosdev/cookingrecipesmvp/mvp/presenter/SearchPresenter.java
@@ -7,6 +7,7 @@ import com.mir.panosdev.cookingrecipesmvp.mvp.model.recipes.Recipe;
 import com.mir.panosdev.cookingrecipesmvp.mvp.model.recipes.RecipesResponse;
 import com.mir.panosdev.cookingrecipesmvp.mvp.view.SearchActivityMVP;
 
+import java.net.HttpURLConnection;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -48,9 +49,15 @@ public class SearchPresenter implements SearchActivityMVP.Presenter {
                     .subscribeWith(new DisposableObserver<Response<RecipesResponse>>() {
                         @Override
                         public void onNext(Response<RecipesResponse> recipesResponseResponse) {
-                            List<Recipe> recipes = mRecipeMapper.mapResults(recipesResponseResponse.body().getRecipes());
-                            mView.onClearItems();
-                            mView.onRecipeLoaded(recipes);
+                            if(recipesResponseResponse.isSuccessful()) {
+                                List<Recipe> recipes = mRecipeMapper.mapResults(recipesResponseResponse.body().getRecipes());
+                                mView.onClearItems();
+                                mView.onRecipeLoaded(recipes);
+                            }
+                            else if(recipesResponseResponse.code() == HttpURLConnection.HTTP_NOT_FOUND){
+                                mView.onHideDialog();
+                                mView.onShowToast("Recipe not found.");
+                            }
                         }
 
                         @Override
